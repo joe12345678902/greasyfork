69ee24f0745710c99999836ce6756c60adff0604
joe12345678902 <joshkummer77@gmail.com>
2015-11-25 13:40:13 -0500

    Update moderator_actions_controller.rb

--- app/controllers/moderator_actions_controller.rb
+++ app/controllers/moderator_actions_controller.rb
@@ -1,7 +1,7 @@
 class ModeratorActionsController < ApplicationController
 
 	def index
-		@actions = ModeratorAction.includes([:script, :moderator, :user]).order('id desc').paginate(:page => params[:page], :per_page => 100)
+		@actions = ModeratorAction.includes([:script, :moderator, :user, :mass]).order('id desc').paginate(:page => params[:page], :per_page => 100)
 		@canonical_params = [:page]
 	end
 end
